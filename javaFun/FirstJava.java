b8d1e98bdf9ee0369d7625ba4e05b82937694e0a
elizabeth-reber <elizabeth.szathmary@gmail.com>
2021-01-14 12:32:01 -0800

    first Java program

---
+++ javaFun/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My Name is Elizabeth Reber");
+        System.out.println("I am 42 years old");
+        System.out.println("My hometown is Copperopolis, CA");
+    }
+}
