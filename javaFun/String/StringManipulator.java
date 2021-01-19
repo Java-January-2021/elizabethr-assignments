a9cc115e37e256c615c51e4dfeac8e153e256ff3
elizabeth-reber <elizabeth.szathmary@gmail.com>
2021-01-18 18:16:48 -0800

    stringManipulator

---
+++ javaFun/String/StringManipulator.java
@@ -0,0 +1,41 @@
+public class StringManipulator{
+
+        public String trimAndConcat(String value, String value1 )
+    {
+        value=value.trim();
+        value1=value1.trim();
+        String newvalue=(value+value1).trim();
+        return newvalue;
+    }
+
+    public Integer getIndexOrNull(String value, char M)
+    {
+        Integer value1=value.indexOf(M);
+        if (value1<0)
+        {
+            return null;
+        }
+        return value1;
+    }
+
+    public Integer getIndexOrNull(String value, String value1)
+    {
+        Integer value2=value.indexOf(value1.toCharArray()[0]);
+        if (value2<0)
+        {
+            return null;
+        }
+        return value2;
+    
+    }
+
+    public String Concat(String value, String value1 )
+    {
+        value=value.substring();
+        value1=value1.substring();
+        String newvalue=(value+value1).substring();
+        return newvalue;
+    }
+
+}
+
